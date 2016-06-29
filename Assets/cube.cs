804c771714b4d798e6c77b65cc941c01e0b1a239
shangrilacreate <y.shima9800@gmail.com>
2016-06-29 17:41:05 +0900

    メッセージを修正

--- Assets/cube.cs
+++ Assets/cube.cs
@@ -5,7 +5,7 @@ public class cube : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Hello! GitHub!");
+        Debug.Log("Hello! GitHub!!");
 	}
 	
 	// Update is called once per frame
